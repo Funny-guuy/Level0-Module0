29666e2248afa4b8f450c8c27828b83073ed943b
lvl0_module2 <aaritrj@gmail.com>
2024-08-04 12:52:20 -0700

    w

--- _02_code_flow/_b_pentagon_crazy.py
+++ _02_code_flow/_b_pentagon_crazy.py
@@ -33,21 +33,28 @@ if __name__ == '__main__':
     # Create a variable to be the angle of 360 divided by the sides variable
     angle = 360 / sides
     # Use a for loop to repeat ALL the following lines of code 360 times. 
-    for i in range(360000000):
+    for i in range(36000):
         # If the loop variable (i) is equal to 100, set the turtle width to 2
         if i == 100:
             turtleme.width(2)
         # If the loop variable (i) is equal to 200, set the turtle width to 3
         if i == 200:
             turtleme.width(3)
-
+        if i == 300:
+            turtleme.width(4)
+        if i == 400:
+            turtleme.width(5)
+        if i == 500:
+            turtleme.width(6)
+        if i == 600:
+            turtleme.width(7)
         # Use the get_next_color function to set the turtle pencolor,
         # *hint .pencolor(get_next_color(i))
         turtleme.pencolor(get_next_color(i))
         # Move the turtle forward by the loop variable, *hint .forward(i)
         turtleme.forward(i)
         # Turn the turtle to the right by the angle variable + 1
-        turtleme.right(angle + sides)
+        turtleme.right(angle + 1)
     # Hide your turtle so you can see the pattern.
         turtleme.hideturtle()
     # Check the pattern against the picture in the recipe. If it matches, you are done!
